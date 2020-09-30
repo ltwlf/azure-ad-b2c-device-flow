e5814a52637377f040d32a2629a030368fa2c86b
Christian Glessner <chris@leitwolf.io>
2020-09-30 19:26:16 +0200

    Fixes typo in json token response

--- src/DeviceAuthService/Token.cs
+++ src/DeviceAuthService/Token.cs
@@ -15,7 +15,7 @@ namespace Ltwlf.Azure.B2C
     {
         private class TokenResponse
         {
-            [JsonProperty("access_Token")] public string AccessToken { get; set; }
+            [JsonProperty("access_token")] public string AccessToken { get; set; }
             [JsonProperty("token_type")] public string TokenType { get; set; }
             [JsonProperty("expires_in")] public int ExpiresIn { get; set; }
             [JsonProperty("refresh_token")] public string RefreshToken { get; set; }
