61056ee448daf89c97ad6681afbd01556ddc2f82
Christian Glessner <chris@leitwolf.io>
2020-09-30 19:26:16 +0200

    Fixes typo in json token response

--- src/DeviceAuthService/Token.cs
+++ src/DeviceAuthService/Token.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using StackExchange.Redis;
+using MediaTypeHeaderValue = System.Net.Http.Headers.MediaTypeHeaderValue;
 
 namespace Ltwlf.Azure.B2C
 {
@@ -19,15 +24,18 @@ namespace Ltwlf.Azure.B2C
             [JsonProperty("token_type")] public string TokenType { get; set; }
             [JsonProperty("expires_in")] public int ExpiresIn { get; set; }
             [JsonProperty("refresh_token")] public string RefreshToken { get; set; }
-            
             [JsonProperty("scope")] public string Scope { get; set; }
         }
 
         private readonly IConnectionMultiplexer _muxer;
+        private readonly HttpClient _client;
+        private readonly IConfiguration _config;
 
-        public Token(IConnectionMultiplexer muxer)
+        public Token(IConnectionMultiplexer muxer, HttpClient client, IConfiguration config)
         {
             _muxer = muxer;
+            _client = client;
+            _config = config;
         }
 
         [FunctionName("Token")]
@@ -42,9 +50,40 @@ namespace Ltwlf.Azure.B2C
             if (deviceCode == null || grantType == null || clientId == null)
                 return new BadRequestObjectResult("device_code, client_id, grant_type are mandatory");
 
-            if (!grantType.Equals("urn:ietf:params:oauth:grant-type:device_code", StringComparison.OrdinalIgnoreCase))
+            /*
+            if (!grantType.Equals("urn:ietf:params:oauth:grant-type:device_code", StringComparison.OrdinalIgnoreCase) || !grantType.Equals("refresh_token", StringComparison.OrdinalIgnoreCase) )
                 return new BadRequestObjectResult("gran_type must be \"urn:ietf:params:oauth:grant-type:device_code\"");
-
+*/
+            if (grantType.Equals("refresh_token", StringComparison.OrdinalIgnoreCase))
+            {
+                
+                var tenant = _config.GetValue<string>("B2CTenant");
+                var signInFlow = _config.GetValue<string>("SignInFlow");
+                var appId = _config.GetValue<string>("AppId");
+                var appSecret = _config.GetValue<string>("AppSecret");
+                var scope = req.Form["scope"].SingleOrDefault() ?? "openid";
+                var refreshToken = req.Form["refresh_token"];
+                
+                var b2CTokenResponse = await _client.PostAsync($"{tenant}/{signInFlow}/oauth2/v2.0/token",
+                    new StringContent(
+                            $"grant_type=refresh_token&refresh_token={refreshToken}&client_id={appId}&client_secret={appSecret}&scope={scope}&")
+                        {Headers = {ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded")}});
+                
+                var jwt = await b2CTokenResponse.Content.ReadAsAsync<JObject>();
+                
+                var accessTokenResponse = new TokenResponse
+                {
+                    AccessToken = jwt.Value<string>("access_token"),
+                    RefreshToken = jwt.Value<string>("refresh_token"),
+                    ExpiresIn = jwt.Value<int>("expires_in"),
+                    TokenType = jwt.Value<string>("token_type"),
+                    Scope =  jwt.Value<string>("token_type")
+                };
+                
+                return new OkObjectResult(accessTokenResponse);
+                 
+            }
+            
             var pattern = $"{deviceCode}:*";
 
             var key = await Helpers.GetKeyByPattern(_muxer, pattern);
